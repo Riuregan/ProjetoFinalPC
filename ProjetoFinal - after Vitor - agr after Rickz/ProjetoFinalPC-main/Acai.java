8ccba31277dc3544b1a85f8aeaa612304be1880c
Henrique <72466642+Riuregan@users.noreply.github.com>
2021-04-12 22:19:17 -0300

    Add files via upload

---
+++ ProjetoFinal - after Vitor - agr after Rickz/ProjetoFinalPC-main/Acai.java
@@ -0,0 +1,58 @@
+public class Acai extends Produto {
+
+    private double valor = 0.0;
+    Compra gasto = new Compra();
+    // private String nomeSabor = "";
+
+    public Acai(int quantidade, String tamanho, int sabor) {
+        super(quantidade, tamanho, sabor);
+    }
+
+    @Override
+    public double calcularPreco() {
+        if (sabor == 1) {
+            // this.nomeSabor = "Acaí tradicional";
+            if (tamanho.equals("g") || tamanho.equals("G")) {
+                valor = 34.20 * quantidade;
+            } else if (tamanho.equals("m") || tamanho.equals("M")) {
+                valor = 27.40 * quantidade;
+            } else if (tamanho.equals("p") || tamanho.equals("P")) {
+                valor = 23.50 * quantidade;
+            }
+        }
+
+        if (sabor == 2) {
+            // this.nomeSabor = "Acaí com leite";
+            if (tamanho.equals("g") || tamanho.equals("G")) {
+                valor = 35.20 * quantidade;
+
+            } else if (tamanho.equals("m") || tamanho.equals("M")) {
+                valor = 28.40 * quantidade;
+
+            } else if (tamanho.equals("p") || tamanho.equals("P")) {
+                valor = 24.50 * quantidade;
+
+            }
+        }
+
+        if (sabor == 3) {
+            // this.nomeSabor = "Suco de acaí";
+            if (tamanho.equals("g") || tamanho.equals("G")) {
+                valor = 35.20 * quantidade;
+
+            } else if (tamanho.equals("m") || tamanho.equals("M")) {
+                valor = 28.40 * quantidade;
+
+            } else if (tamanho.equals("p") || tamanho.equals("P")) {
+                valor = 24.50 * quantidade;
+            }
+        }
+        return valor;
+    }
+
+    @Override
+    public double ultimoGasto() {
+        return valor;
+    }
+
+}
